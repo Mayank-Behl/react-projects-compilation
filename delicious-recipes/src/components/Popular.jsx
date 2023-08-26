db91109a3cb5fc255f042c9945c8c6ab85bb61ed
Mayank-Behl <37586705+Mayank-Behl@users.noreply.github.com>
2023-08-26 23:47:08 +0530

    Popular section, API call and populating the data via api

--- delicious-recipes/src/components/Popular.jsx
+++ delicious-recipes/src/components/Popular.jsx
@@ -1,5 +1,49 @@
-const Popular = () => {
-  return <div>Popular</div>;
-};
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+function Popular() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=52249af270bd4a6aab23c922c21f7675&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+    console.log(data.recipes);
+  };
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+              return (
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              );
+            })}
+          </Wrapper>
+        );
+      })}
+    </div>
+  );
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+`;
 
 export default Popular;
