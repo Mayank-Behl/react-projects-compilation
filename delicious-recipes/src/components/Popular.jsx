192fa816442265a51ae1f3e7476b1a5ca483b1d7
Mayank-Behl <37586705+Mayank-Behl@users.noreply.github.com>
2023-08-27 15:43:33 +0530

    Vegetarian section added

--- delicious-recipes/src/components/Popular.jsx
+++ delicious-recipes/src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
+import { Wrapper, Card, Gradient } from "./StyledComponents";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -66,49 +66,49 @@ function Popular() {
   );
 }
 
-const Wrapper = styled.div`
-  margin: 4rem 0rem;
-`;
+// const Wrapper = styled.div`
+//   margin: 4rem 0rem;
+// `;
 
-const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
+// const Card = styled.div`
+//   min-height: 25rem;
+//   border-radius: 2rem;
+//   overflow: hidden;
+//   position: relative;
 
-  img {
-    border-radius: 2rem;
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+//   img {
+//     border-radius: 2rem;
+//     position: absolute;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     object-fit: cover;
+//   }
 
-  p {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
+//   p {
+//     position: absolute;
+//     z-index: 10;
+//     left: 50%;
+//     bottom: 0%;
+//     transform: translate(-50%, 0%);
+//     color: white;
+//     width: 100%;
+//     text-align: center;
+//     font-weight: 600;
+//     font-size: 1rem;
+//     height: 40%;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//   }
+// `;
 
-const Gradient = styled.div`
-  z-index: 3;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
-`;
+// const Gradient = styled.div`
+//   z-index: 3;
+//   position: absolute;
+//   width: 100%;
+//   height: 100%;
+//   background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+// `;
 
 export default Popular;
--- delicious-recipes/src/components/Veggie.jsx
+++ delicious-recipes/src/components/Veggie.jsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { Wrapper, Card, Gradient } from "./StyledComponents";
+
 const Veggie = () => {
-  return <div>Veggie</div>;
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      try {
+        const parsedData = JSON.parse(check);
+        setVeggie(parsedData);
+      } catch (error) {
+        console.error("Error parsing data from localStorage:", error);
+        localStorage.removeItem("veggie");
+      }
+    } else {
+      try {
+        const api = await fetch(
+          `https://api.spoonacular.com/recipes/random?apiKey=52249af270bd4a6aab23c922c21f7675&number=9&tags=vegetarian`
+        );
+        const data = await api.json();
+
+        localStorage.setItem("veggie", JSON.stringify(data.recipes));
+        setVeggie(data.recipes);
+        console.log(data.recipes);
+      } catch (error) {
+        console.error("Error fetching data from the API:", error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Our Vegitarian Picks</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            pagination: false,
+            arrows: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {veggie.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Veggie;
